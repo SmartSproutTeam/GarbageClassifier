fd1f516179a4fe48c6706756eca1e2d0e71938eb
charlie <charlie275691502@gmail.com>
2025-04-08 00:37:26 +1000

    test dataset artifact

---
+++ Model/clearML/step1_dataset_artifact.py
@@ -0,0 +1,18 @@
+from clearml import Task
+
+# Create a dataset experiment
+task = Task.init(project_name="examples", task_name="Pipeline step 1 dataset artifact")
+
+# Only create the task, we will actually execute it later
+task.execute_remotely()
+
+# Path to the zipped dataset file
+zipped_dataset = "../../Data/archive.zip"
+
+# Upload the zipped file as an artifact
+task.upload_artifact(name="dataset", artifact_object=zipped_dataset)
+
+print(f"Uploaded zipped dataset '{zipped_dataset}' as an artifact.")
+
+# We are done
+print("Done")
